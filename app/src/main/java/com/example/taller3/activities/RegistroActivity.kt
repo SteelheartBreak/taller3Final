4c14b661015b3f4774f4a5c2210f42bc7ed6b399
Lukas Rodriguez <lukasrodriguez1997@gmail.com>
2023-11-06 19:21:48 -0500

    se agrega la funcionalidad para traer la ubicacion para el registro

--- app/src/main/java/com/example/taller3/activities/RegistroActivity.kt
+++ app/src/main/java/com/example/taller3/activities/RegistroActivity.kt
@@ -1,12 +1,23 @@
 package com.example.taller3.activities
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.taller3.databinding.ActivityRegistroBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.parse.ParseException
 import com.parse.ParseObject
 import com.parse.ParseUser
@@ -20,6 +31,14 @@ class RegistroActivity : AppCompatActivity() {
     var confirmarContrasena = ""
     var apellido =""
     var numIdentificacion =""
+    lateinit var lastLocation : Location // Última ubicación conocida
+    private val permisosUbicacionRequestCode = 123 // Identificador único para la solicitud de permisos
+
+
+
+    lateinit var locationClient: FusedLocationProviderClient // Cliente de ubicación
+    lateinit var locationRequest : LocationRequest // Solicitud de ubicación
+    lateinit var locationCallback: LocationCallback // Callback de ubicación
 
     val TAG = "GREETING_APP"
     val USER_CN = "Usuario"
@@ -29,6 +48,13 @@ class RegistroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistroBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        locationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = createLocationRequest()
+        locationCallback = createLocationCallBack()
+
+        // Verificar si ya se tienen permisos. si se tienen, se inicia la actualizacion de ubicacion.
+        startLocationUpdates()
 
         setupRegisterButton()
 
@@ -110,5 +136,49 @@ class RegistroActivity : AppCompatActivity() {
     fun isValidEmail(email: String): Boolean {
         val emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$".toRegex()
         return email.matches(emailRegex)
+    }
+
+    fun createLocationRequest() : LocationRequest{
+        locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000)
+            .setWaitForAccurateLocation(true)
+            .setMinUpdateIntervalMillis(3000)
+            .build()
+        return locationRequest
+    }
+
+    fun createLocationCallBack() : LocationCallback{
+        val locationCallback = object : LocationCallback(){
+            override fun onLocationResult(result: LocationResult) {
+                super.onLocationResult(result)
+                if(result!=null){
+                    lastLocation = result.lastLocation!!
+                }
+            }
+        }
+        return locationCallback
+    }
+
+    fun startLocationUpdates(){
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestLocationPermission()
+        }else{
+            locationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+        }
+    }
+
+    // Función para solicitar permisos de ubicacion al usuario
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+            permisosUbicacionRequestCode
+        )
     }
 }

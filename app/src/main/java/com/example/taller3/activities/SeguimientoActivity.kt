fade0f13148de216a1ed8af9df98b5b073fbaccc
SteelheartBreak <moisesd.quintero@javeriana.edu.co>
2023-11-07 21:53:39 -0500

    actualizacion Seguimiento

--- app/src/main/java/com/example/taller3/activities/SeguimientoActivity.kt
+++ app/src/main/java/com/example/taller3/activities/SeguimientoActivity.kt
@@ -1,16 +1,63 @@
 package com.example.taller3.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.taller3.R
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.taller3.databinding.ActivitySeguimientoBinding
+import com.parse.ParseQuery
+import com.parse.ParseUser
+import com.parse.livequery.ParseLiveQueryClient
+import com.parse.livequery.SubscriptionHandling
+
 
 class SeguimientoActivity : AppCompatActivity() {
     lateinit var binding: ActivitySeguimientoBinding
+
+    lateinit var parseLiveQueryClient: ParseLiveQueryClient
+    lateinit var parseQuery: ParseQuery<ParseUser>
+    lateinit var idSeguir :String
+    var latitudSeguir : Double = 0.0
+    var longitudSeguir : Double = 0.0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivitySeguimientoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val idSeguir = intent.getStringExtra("objectID")
+        println("Se ha iniciado el seguimiento a: "+idSeguir)
+
+        initParseLiveQuery()
+        setupSubscription()
+    }
+
+    private fun initParseLiveQuery() {
+        parseLiveQueryClient = ParseLiveQueryClient.Factory.getClient()
+    }
+
+    private fun setupSubscription() {
+
+        parseQuery = ParseUser.getQuery().whereEqualTo("objectId", idSeguir)
+
+        // Subscribirse a los cambios
+        val subscriptionHandling = parseLiveQueryClient.subscribe(parseQuery)
+
+        // Reaccionar a los cambios en las columnas específicas
+        subscriptionHandling.handleEvent(SubscriptionHandling.Event.UPDATE) { _, user ->
+            user?.let {
+                if (it.has("latitud") && it.has("longitud")) {
+                    val latitud = it.getDouble("latitud")
+                    val longitud = it.getDouble("longitud")
+                    locationChanged(latitud, longitud)
+                }
+            }
+        }
+    }
+
+    // Método que maneja los cambios en la ubicación
+    private fun locationChanged(latitud: Double, longitud: Double) {
+        // Actualizar la interfaz de usuario o lógica de la aplicación según sea necesario
+        Log.i("LocationChange", "Ubicación actualizada: Latitud $latitud, Longitud $longitud")
     }
 }
--- app/src/main/java/com/example/taller3/adapters/UsersAdapter.kt
+++ app/src/main/java/com/example/taller3/adapters/UsersAdapter.kt
@@ -1,17 +1,21 @@
 package com.example.taller3.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.database.Cursor
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.CursorAdapter
 import android.widget.ImageView
 import android.widget.TextView
 import com.example.taller3.R
 import com.example.taller3.model.Usuario
 import com.bumptech.glide.Glide
+import com.example.taller3.activities.RegistroActivity
+import com.example.taller3.activities.SeguimientoActivity
 import kotlinx.coroutines.CoroutineScope
 
 class UsersAdapter(context: Context, userList: List<Usuario>) :
@@ -28,6 +32,7 @@ class UsersAdapter(context: Context, userList: List<Usuario>) :
 
         val nombreTextView = listItemView?.findViewById<TextView>(R.id.contactName)
         val fotoImageView = listItemView?.findViewById<ImageView>(R.id.fotoP)
+        val followButton = listItemView?.findViewById<Button>(R.id.btnFollow)
 
         nombreTextView?.text = usuario?.usuario
         val imageUrl = usuario?.urlImagen
@@ -36,6 +41,14 @@ class UsersAdapter(context: Context, userList: List<Usuario>) :
         // Ejemplo ficticio, debes implementar la carga de la imagen desde la URI
         if (fotoImageView != null) {
             Glide.with(context).load(imageUrl).into(fotoImageView)
+        }
+
+        // Configura el OnClickListener para el botón
+        followButton?.setOnClickListener {
+            // Inicia DetailActivity y pasa el objectID como un extra
+            val intent = Intent(context, SeguimientoActivity::class.java)
+            intent.putExtra("objectID", usuario?.objectID)
+            context.startActivity(intent)
         }
 
         return listItemView!!
